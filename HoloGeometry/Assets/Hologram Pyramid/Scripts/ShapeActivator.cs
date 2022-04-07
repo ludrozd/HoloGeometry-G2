49a0f294e1e5d45e4d0c63f24164803eeb8d5737
mtikvica <mato.tikvica@gmail.com>
2022-04-07 11:43:07 +0200

    Recognize

--- HoloGeometry/Assets/Hologram Pyramid/Scripts/ShapeActivator.cs
+++ HoloGeometry/Assets/Hologram Pyramid/Scripts/ShapeActivator.cs
@@ -1,11 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ShapeActivator : MonoBehaviour
 {
     public GameObject[] geomObject;
-    // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < geomObject.Length; i++)
